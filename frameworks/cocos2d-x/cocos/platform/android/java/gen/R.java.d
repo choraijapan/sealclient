e8c3ef8a54de37e0fc72168489dd712afe5962e3
chorai <cho.rai.japan@gmail.com>
2015-12-02 20:46:18 +0900

    [develop][build] Android

--- frameworks/cocos2d-x/cocos/platform/android/java/gen/R.java.d
+++ frameworks/cocos2d-x/cocos/platform/android/java/gen/R.java.d
@@ -1,1 +1,1 @@
- : /Users/Allproject/working/01_project/crown-client/frameworks/cocos2d-x/cocos/platform/android/java/bin/AndroidManifest.xml \
+ : /Users/harai/Working/cl_project/crown-client/frameworks/cocos2d-x/cocos/platform/android/java/bin/AndroidManifest.xml \
