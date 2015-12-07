590af745037163eac37ddea0943b6d659b0d1220
chorai <cho.rai.japan@gmail.com>
2015-12-07 17:03:22 +0900

    [develop][puzzle] UIの変更

--- frameworks/cocos2d-x/cocos/platform/android/java/gen/R.java.d
+++ frameworks/cocos2d-x/cocos/platform/android/java/gen/R.java.d
@@ -1,1 +1,1 @@
- : /Users/harai/Working/cl_project/crown-client/frameworks/cocos2d-x/cocos/platform/android/java/bin/AndroidManifest.xml \
+ : /Users/Allproject/working/01_project/crown-client/frameworks/cocos2d-x/cocos/platform/android/java/bin/AndroidManifest.xml \
