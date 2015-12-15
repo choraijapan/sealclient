fa32f6174ab84daaaa0f95bcb1579f43753ade46
MineMasahiro <minemasahiro@fengzhengbos-MacBook-Air.local>
2015-12-15 21:54:57 +0900

    temp

--- src/app/network/api/BaseApi.lua
+++ src/app/network/api/BaseApi.lua
@@ -1,22 +1,22 @@
-local msgPack = require("msgpackcpp")
+
     
 local BaseApi = class("BaseApi")
 
 function BaseApi:request(data)
     local data = {}
 	local url = "http://www.baidu.com/"
---	NetworkManager:request("http://www.baidu.com/",data)
---	NetworkManager:request("http://www.yahoo.co.jp/",data)
---	NetworkManager:request("http://www.rakuten.co.jp/",data)
+	NetworkManager:request("http://www.baidu.com/",data)
+	NetworkManager:request("http://www.yahoo.co.jp/",data)
+	NetworkManager:request("http://www.rakuten.co.jp/",data)
 --	
-	local data1 = {
-	   a = 1,
-	   b = "お破y路Fだfdさfdさfdさfdさfdさ"
-	}
-	
-	local tbl = { a=123, b="any", c={"ta","bl","e",1,2,3} }
-	local packed = msgPack.pack(data1)
-	local unpacked_table = msgPack.unpack(packed)
+--	local data1 = {
+--	   a = 1,
+--	   b = "お破y路Fだfdさfdさfdさfdさfdさ"
+--	}
+--	
+--	local tbl = { a=123, b="any", c={"ta","bl","e",1,2,3} }
+--	local packed = msgPack.pack(data1)
+--	local unpacked_table = msgPack.unpack(packed)
 	
 end
 
--- src/core/manager/NetworkManager.lua
+++ src/core/manager/NetworkManager.lua
@@ -1,34 +1,36 @@
 NetworkManager = class("NetworkManager")
+local msgPack = require("msgpackcpp")
 
 function NetworkManager:request(url,data)
 
 	local xhr = cc.XMLHttpRequest:new()
-	xhr.responseType = cc.XMLHTTPREQUEST_RESPONSE_STRING --cc.XMLHTTPREQUEST_RESPONSE_MSGPACK
+	xhr.responseType = cc.XMLHTTPREQUEST_RESPONSE_MSGPACK
 	xhr:open("POST", url)
 
 	local function onResponse()
 		if xhr.readyState == 4 and (xhr.status >= 200 and xhr.status < 207) then
 			local response   = xhr.response
-			print(response)
+			local test = 1
 		else
-			print("xhr.readyState is:", xhr.readyState, "xhr.status is: ",xhr.status)
+			-- error
+			DebugLog.error("xhr.readyState is:", xhr.readyState, "xhr.status is: ",xhr.status)
 		end
 	end
 
 	xhr:registerScriptHandler(onResponse)
-	xhr:send("aaa")
+	xhr:send(msgPack.pack(data))
 
---	local xhr = cc.XMLHttpRequest:new()
---	xhr.responseType = cc.XMLHTTPREQUEST_RESPONSE_STRING
---	xhr:open("POST", "http://www.baidu.com/")
---	local function onReadyStateChange()
---		if xhr.readyState == 4 and (xhr.status >= 200 and xhr.status < 207) then
---			print(xhr.response)
---		else
---			print("xhr.readyState is:", xhr.readyState, "xhr.status is: ",xhr.status)
---		end
---	end
---	xhr:registerScriptHandler(onReadyStateChange)
---	xhr:send("aaa")
+	--	local xhr = cc.XMLHttpRequest:new()
+	--	xhr.responseType = cc.XMLHTTPREQUEST_RESPONSE_STRING
+	--	xhr:open("POST", "http://www.baidu.com/")
+	--	local function onReadyStateChange()
+	--		if xhr.readyState == 4 and (xhr.status >= 200 and xhr.status < 207) then
+	--			print(xhr.response)
+	--		else
+	--			print("xhr.readyState is:", xhr.readyState, "xhr.status is: ",xhr.status)
+	--		end
+	--	end
+	--	xhr:registerScriptHandler(onReadyStateChange)
+	--	xhr:send("aaa")
 
 end
