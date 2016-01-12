f87b102685ccc4d8a59a10e4c5acf31ae76c12a7
MineMasahiro <minemasahiro@fengzhengbos-MacBook-Air.local>
2016-01-12 22:33:13 +0900

    add android mk

--- frameworks/cocos2d-x/extensions/Android.mk
+++ frameworks/cocos2d-x/extensions/Android.mk
@@ -187,7 +187,8 @@ LOCAL_STATIC_LIBRARIES += box2d_static
 LOCAL_CXXFLAGS += -fexceptions
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH) \
-					$(LOCAL_PATH)/../../runtime-src/plugins 
+					$(LOCAL_PATH)/../../runtime-src/plugins \
+					$(LOCAL_PATH)/../../runtime-src/plugins/wxsqlite3/src 
 
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/. \
                            $(LOCAL_PATH)/.. \
